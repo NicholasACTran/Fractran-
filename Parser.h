581ce74da4ff316f9f8bab619c03b5bdd4252990
NicholasACTran <nicholas.ac.tran@gmail.com>
2017-04-03 21:44:07 -0700

    Split parsing functions into separate .cpp and .h
    Improves readibility and allows for more complex changes to parsing in
    the future.

---
+++ Parser.h
@@ -0,0 +1,23 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+//This file holds all of the function declarations for the functions used to parse
+//the Fractran program and create the data structure for execution.
+
+void parse_Literals(queue<string> &FractranLiterals, string a);
+int asterisk_parse(queue<string> &FractranLiterals, vector<int> &Multiplication, vector<int> &Exponentiation);
+int carrot_parse (queue<string> &FractranLiterals, vector<int> &Multiplication, vector<int> Exponentiation);
+int left_parenthesis_parse(queue<string> &FractranLiterals, vector<int> &FractranIntegers);
+void parse(queue<string> &FractranLiterals, vector<int> &FractranIntegers);
+
+
+
+#endif
